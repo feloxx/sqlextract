2ba260c5bb702dd0e8ebe4b04f5a8c73df586674
chendapao <feloxx@163.com>
2019-06-29 17:12:23 +0800

    add ddl column

---
+++ extract/ddl.go
@@ -0,0 +1,43 @@
+package extract
+
+import (
+	"github.com/pingcap/parser/ast"
+	. "github.com/pingcap/parser/format"
+	"strings"
+)
+
+type DdlVisitor struct {
+	sb         strings.Builder
+	ddlColumns []string
+}
+
+func (v *DdlVisitor) Enter(in ast.Node) (out ast.Node, skipChildren bool) {
+	switch n := in.(type) {
+	case *ast.CreateTableStmt:
+		for _, col := range n.Cols {
+			err := col.Restore(NewRestoreCtx(DefaultRestoreFlags, &v.sb))
+			if err != nil {
+				panic(err)
+			}
+			v.ddlColumns = append(v.ddlColumns, v.sb.String())
+			v.sb.Reset()
+		}
+	}
+	return in, false
+}
+
+func (v *DdlVisitor) Leave(in ast.Node) (out ast.Node, ok bool) {
+	return in, true
+}
+
+func (v *DdlVisitor) Parser(sql string) []string {
+	stmts, _, _ := p.Parse(sql, "", "")
+	for _, stmt := range stmts {
+		stmt.Accept(v)
+	}
+	return v.ddlColumns
+}
+
+func NewDDL() *DdlVisitor {
+	return &DdlVisitor{}
+}
